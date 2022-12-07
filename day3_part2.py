1be5e850b105644a12c1bb0e8a8296093fd8357a
zjardyn <zjardyn@gmail.com>
2022-12-06 23:12:31 -0500

    day 3 done

--- day3_part2.py
+++ day3_part2.py
@@ -2,7 +2,15 @@ from aocd import get_data, submit
 
 data = get_data(day=3, year=2022).splitlines()
 
-  
-# if __name__ == '__main__':                                                             
-#     submit(sum, part = 1, day = 3, year = 2022)
- 
+groups = [data[i:i+3] for i in range(0, len(data), 3)]
+
+sum = 0
+for i in groups:
+    common = list(set.intersection(*map(set, i)))[0]
+    if common.isupper():
+        sum += ord(common) - 38 
+    else:
+        sum += ord(common) - 96
+
+if __name__ == '__main__':                                                             
+    submit(sum, part = 2, day = 3, year = 2022)
